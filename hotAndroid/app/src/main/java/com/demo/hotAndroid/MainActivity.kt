9d6e1b4113f31226aed67138227030da214805c0
Abhimanyu Singh <abhimanyu.new@gmail.com>
2020-10-04 21:43:24 +0100

    Added a navigation graph

--- hotAndroid/app/src/main/java/com/demo/hotAndroid/MainActivity.kt
+++ hotAndroid/app/src/main/java/com/demo/hotAndroid/MainActivity.kt
@@ -2,17 +2,23 @@ package com.demo.hotAndroid
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import androidx.fragment.app.Fragment
 import com.demo.hotAndroid.ui.main.PostsListFragment
+import dagger.android.AndroidInjector
+import dagger.android.DispatchingAndroidInjector
+import dagger.android.HasAndroidInjector
+import javax.inject.Inject
 
-class MainActivity : AppCompatActivity() {
+class MainActivity : AppCompatActivity(), HasAndroidInjector  {
+
+    @Inject
+    lateinit var dispatchingAndroidInjector: DispatchingAndroidInjector<Any>
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.main_activity)
-        if (savedInstanceState == null) {
-            supportFragmentManager.beginTransaction()
-                    .replace(R.id.container, PostsListFragment.newInstance())
-                    .commitNow()
-        }
     }
+
+    override fun androidInjector() = dispatchingAndroidInjector
+
 }
